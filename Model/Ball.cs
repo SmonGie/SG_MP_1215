a7223ca05a2bf7831982c55f3cdf0356d73b1697
Mateusz Pawlak <245899@edu.p.lodz.pl>
2024-04-12 23:57:25 +0200

    Naprawa_sln

---
+++ Model/Ball.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    internal class Ball
+    {
+        public Ball(double vertical, double horizontal)
+        {
+            X = horizontal;
+            Y = vertical;
+
+        }
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Radius { get; set; }
+    
+
+
+    private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ Model/ModelAbstractApi.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public interface IBillardBall : INotifyPropertyChanged
+    {
+        double vertical { get; }
+        double horizontal { get; }
+        double Radius { get; }
+    }
+
+    public class DynamicChangeArgs : EventArgs
+    {
+        public IBillardBall Ball { get; internal set; }
+    }
+}
