f176a74a6ad739639b364ea412114728c5ad9e8a
Mateusz Pawlak <245899@edu.p.lodz.pl>
2024-04-13 21:25:03 +0200

    Data_finish

---
+++ Data/AbstractDataApi.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public abstract class AbstractDataApi
+    {
+        public abstract int BallRadius { get; }
+        public abstract int HeightWindow { get; }
+        public abstract int WidthWindow { get; }
+
+
+        // Zdefiniuj metodę statyczną, aby utworzyć wystąpienie klasy AbstractDataAPI.
+        public static AbstractDataApi CreateInstance()
+        {
+            // Zwraca nową instancję klasy Data.
+            return new Data();
+        }
+    }
+}
---
+++ Data/Data.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    internal class Data : AbstractDataApi
+    {
+        public override int BallRadius => 20;
+
+        public override int HeightWindow => 500;
+
+      
+        public override int WidthWindow => 800;
+
+     
+        
+    }
+}
