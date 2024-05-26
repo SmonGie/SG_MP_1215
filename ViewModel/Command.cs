61c04d487a9e0165594507123de54c322bb3b7b3
Mateusz Pawlak <245899@edu.p.lodz.pl>
2024-05-26 21:17:56 +0200

    final_viewmodel_layer

--- ViewModel/Command.cs
+++ ViewModel/Command.cs
@@ -4,19 +4,20 @@ namespace ViewModel
 {
     public class RelayCommand : ICommand
     {
-        public event EventHandler CanExecuteChanged;
+        public event EventHandler CanExecuteChanged; // Zdarzenie wywoływane przy zmianie możliwości wykonania polecenia
 
-        private readonly Action _execute;
-        private readonly Func<bool> _canExecute;
+        private readonly Action _execute; // Delegat akcji do wykonania
+        private readonly Func<bool> _canExecute; // Delegat funkcji sprawdzającej możliwość wykonania polecenia
 
         public RelayCommand(Action execute, Func<bool> canExecute = null)
         {
-            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));  // Sprawdzenie, czy podano metodę do wykonania polecenia
             _canExecute = canExecute;
         }
 
         public bool CanExecute(object parameter)
         {
+            // Zwraca true, jeśli nie ma metody określającej możliwość wykonania polecenia lub zwraca wartość z metody _canExecute()
             return _canExecute == null || _canExecute();
         }
 
